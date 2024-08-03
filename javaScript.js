821b70b74d487c8d29bce4c334e5f80d6dd1f3b1
krishnavish9329 <Krishna.vish9329@gmail.com>
2024-08-03 23:33:32 +0530

    add new message 100+ and add massage after win

--- javaScript.js
+++ javaScript.js
@@ -1,18 +1,25 @@
 
 let guessNumber= "none";
+let term=0;
 function startGame()
 {
-    guessNumber =Math.floor(Math.random()*100)
-    console.log(guessNumber)
+    guessNumber =Math.floor(Math.random()*98)
+    console.log(guessNumber)    
 
     const span_tage = document.getElementById('span');
     document.getElementById("input_id").removeAttribute("readonly");
+    document.body.classList.remove("bdchange");
     span_tage.style.color="#00ff00";
     span_tage.innerHTML="you have started a game, enter the number between 0-100";
+
+    document.getElementById("str-but").innerHTML = `Start Game`;
+    document.getElementById("contain").innerHTML = `numbers `;
+    document.body.style.backgroundColor="rgb(0, 57, 86)";
+    term=0;
 }
 
 let guess;
-let term=0;
+
 let takeNumber = ()=>{
     guess = document.getElementById("input_id").value;
 }
@@ -20,22 +27,46 @@ let takeNumber = ()=>{
 function checkGuesses(){
     const span_tage = document.getElementById('span');
     const conatin_number = document.getElementById('contain');
-    conatin_number.innerHTML= conatin_number.innerHTML + guess + " ";
+    ++term;
+
+    if(term>=10)
+    {
+        span_tage.innerHTML = `loss the game please try agein `;
+        document.getElementById("input_id").setAttribute = "readonly";
+        document.body.style.background="red";
+        document.getElementById("str-but").innerHTML = "Try Again"
+        return;
+    }
+
+    if(guess >= 100)
+    {
+        span_tage.innerHTML = "Node:- Enter wrong number";
+        span_tage.style.color="red";
+        return;
+    }
 
     if(guess == guessNumber)
     {
-        span_tage.innerHTML = " you are win";
+        span_tage.innerHTML = `Congratulations you have won this game in ${term} move 😍`;
+        conatin_number.innerHTML= conatin_number.innerHTML + guess + " ";
+        document.body.classList.add("bdchange")
+        document.getElementById("input_id").setAttribute = "readonly";
+        span_tage.style.color="#00ff00";
         return;
     }
     else
     if(guess < guessNumber)
     {
         span_tage.innerHTML =  guess + " is too low";
+        conatin_number.innerHTML= conatin_number.innerHTML + guess + " ";
+        span_tage.style.color="white";
     }
     else
     if(guess > guessNumber)
     {
         span_tage.innerHTML =  guess + " is too high";
+        conatin_number.innerHTML= conatin_number.innerHTML + guess + " ";
+        span_tage.style.color="white";
     }
 
 
